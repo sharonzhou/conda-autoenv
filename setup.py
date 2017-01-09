068430630ec7a83b2dc01cf54e3d2045608372b4
Sharon Zhou <sharonzhou12@gmail.com>
2017-01-08 23:01:17 -0800

    setup configuration initiated

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 from setuptools import setup
 
-setup(name='conda-autoenv',
-      version='1.0',
-      description='Automatic Conda Environment',
-      author='Sharon Zhou',
-      author_email='sharonzpost@gmail.com',
-      url='https://www.python.org/sigs/distutils-sig/',
-      packages=['distutils', 'distutils.command'],
-     )
+setup(
+	name='conda-autoenv',
+	version='0.1',
+	description='Automatic Conda Environment',
+	author='Sharon Zhou',
+	author_email='sharonzpost@gmail.com',
+	url='https://www.python.org/sigs/distutils-sig/',
+	install_requires=['conda'],
+)
