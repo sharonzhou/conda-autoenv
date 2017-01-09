d073d79cfe967ae526e50332ef1354a70e323cbd
Sharon Zhou <sharonzhou12@gmail.com>
2017-01-08 22:34:33 -0800

    initial commit

---
+++ conda_autoenv.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+# conda-autoenv automatically 
+#  (1) activates a conda environment per the environment.yml file in 
+#      a directory, when you enter it
+#  (2) updates the file and deactivates the environment, when you
+#      leave the directory
+#  (3) installs and updates your pip requirements per the 
+#      requirements.txt in the directory as well
+# 
+# To install, add this line to your .bashrc or .bash-profile:
+#
+#       source /path/to/conda_autoenv.sh
+#
+
+function conda_autoenv() {
+  if [ -e "environment.yml" ]; then
+    ENV=$(head -n 1 environment.yml | cut -f2 -d ' ')
+    # Check if you are already in the environment
+    if [[ $PATH != *$ENV* ]]; then
+      # Check if the environment exists
+      if source activate $ENV && [[ $? -eq 0 ]]; then
+        # Set root directory of active environment
+        CONDA_ENVIRONMENT_ROOT="$(pwd)"
+      else
+        echo "Creating conda environment '$ENV' from environment.yml ('$ENV' was not found using 'conda env list')"
+        conda env create -q -f environment.yml
+        echo "'$ENV' successfully created and will automatically activate in this directory"
+        source activate $ENV
+        if [ -e "requirements.txt" ]; then
+          echo "Installing pip requirements from requirements.txt"
+          pip install -q -r requirements.txt
+          echo "Pip requirements successfully installed"
+        fi
+      fi
+    fi
+  elif [[ $PATH = */envs/* ]]\
+    && [[ $(pwd) != $CONDA_ENVIRONMENT_ROOT ]]\
+    && [[ $(pwd) != $CONDA_ENVIRONMENT_ROOT/* ]]
+  then
+    echo "Deactivating conda environment"
+    export PIP_FORMAT=columns
+    echo "Updating conda environment.yml (and pip requirements.txt)"
+    conda env export > $CONDA_ENVIRONMENT_ROOT/environment.yml
+    pip freeze > $CONDA_ENVIRONMENT_ROOT/requirements.txt
+    CONDA_ENVIRONMENT_ROOT=""
+    echo "Successfully updated environment.yml and requirements.txt"
+    source deactivate
+    echo "Deactivated successfully"
+  fi
+}
+
+export PROMPT_COMMAND=conda_autoenv
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='conda-autoenv',
+      version='1.0',
+      description='Automatic Conda Environment',
+      author='Sharon Zhou',
+      author_email='sharonzpost@gmail.com',
+      url='https://www.python.org/sigs/distutils-sig/',
+      packages=['distutils', 'distutils.command'],
+     )
